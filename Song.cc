55808dea7d585635e0d9365a85f50fcb0c25b6ce
Josh Marusek <jm553020@ohio.edu>
2022-02-14 23:07:22 -0500

    make Song stubs

---
+++ Song.cc
@@ -0,0 +1,37 @@
+#include "Song.h"
+
+
+Song::Song() {
+
+}
+std::string Song::get_name()const {
+
+}
+Date Song::get_release()const {
+
+}
+std::string Song::get_artist()const {
+
+}
+
+bool Song::operator == (const Song& other)const {
+
+}
+bool Song::operator != (const Song& other)const {
+
+}
+
+void Song::input(std::istream& ins) {
+
+}
+void Song::output(std::ostream& outs)const {
+
+}
+
+
+std::istream& operator >> (std::istream& ins, Song& s) {
+
+}
+std::ostream& operator << (std::ostream& outs, const Song& s) {
+
+}
