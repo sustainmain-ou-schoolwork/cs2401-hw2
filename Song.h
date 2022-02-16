c8eeaf26afc09f9b038bcc6c9849cc4749a4c6b7
Josh Marusek <jm553020@ohio.edu>
2022-02-15 23:35:33 -0500

    add documentation comments

--- Song.h
+++ Song.h
@@ -15,24 +15,82 @@
 
 class Song{
     public:
+		/**
+		 * @brief Construct a new Song object.
+		 */
 		Song();
+
+		/**
+		 * @brief Get the name of the Song.
+		 * 
+		 * @return the name of the Song
+		 */
 		std::string get_name()const {return name;}
+
+		/**
+		 * @brief Get the release date of the Song.
+		 * 
+		 * @return the release date of the Song
+		 */
 		Date get_release()const {return release;}
+
+		/**
+		 * @brief Get the artist of the Song.
+		 * 
+		 * @return the artist of the Song
+		 */
         std::string get_artist()const {return artist;}
 
+
+		/**
+		 * @brief Compare the Song object to another one.
+		 * 
+		 * @param other the other Song
+		 * @return true if the Songs contain the same data
+		 */
 		bool operator == (const Song& other)const;
+
+		/**
+		 * @brief Compare the Song object to another one.
+		 * 
+		 * @param other the other Song
+		 * @return true if the Songs don't contain the same data
+		 */
 		bool operator != (const Song& other)const;
 
+
+		/**
+		 * @brief Set the Song data based on input from an istream.
+		 * 
+		 * @param ins the istream to get the data from
+		 */
 		void input(std::istream& ins);
+
+		/**
+		 * @brief Output the Song data to an ostream.
+		 * 
+		 * @param outs the ostream to output the data to
+		 */
 		void output(std::ostream& outs)const;
 
     private:
-		std::string name;
-		Date release;
-        std::string artist;
+		std::string name;    // the name of the Song
+		Date release;        // the release date of the Song
+        std::string artist;  // the artist of the Song
 };
 
+/**
+ * @brief Set the Song data based on input from an istream.
+ * 
+ * @param ins the istream to get the data from
+ */
 std::istream& operator >> (std::istream& ins, Song& s);
+
+/**
+ * @brief Output the Song data to an ostream.
+ * 
+ * @param outs the ostream to output the data to
+ */
 std::ostream& operator << (std::ostream& outs, const Song& s);
 
 #endif
