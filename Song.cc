c8ac4712d342198a7436ab041552b8480af21af1
Josh Marusek <jm553020@ohio.edu>
2022-02-15 22:49:32 -0500

    add Song input/output

--- Song.cc
+++ Song.cc
@@ -2,7 +2,9 @@
 
 
 Song::Song() {
-
+    name = "";
+    release = Date();
+    artist = "";
 }
 std::string Song::get_name()const {
 
@@ -22,16 +24,25 @@ bool Song::operator != (const Song& other)const {
 }
 
 void Song::input(std::istream& ins) {
-
+    std::string lineIn;
+    getline(ins, lineIn);
+    name = lineIn;
+    ins >> release;
+    getline(ins, lineIn);
+    artist = lineIn;
 }
 void Song::output(std::ostream& outs)const {
-
+    outs << name << '\n';
+    outs << release << '\n';
+    outs << artist << std::endl;
 }
 
 
 std::istream& operator >> (std::istream& ins, Song& s) {
-
+    s.input(ins);
+    return ins;
 }
 std::ostream& operator << (std::ostream& outs, const Song& s) {
-
+    s.output(outs);
+    return outs;
 }
